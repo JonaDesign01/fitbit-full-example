23deb96478c4621b8412e647bf9b2fb229d3f50f
JonaDesign01 <91673367+JonaDesign01@users.noreply.github.com>
2021-12-08 09:52:21 +0100

    New page library

--- app/pages/index.js
+++ app/pages/index.js
@@ -3,6 +3,7 @@ import { switchPage } from '../navigation';
 
 let $buttonDetail = null;
 let $buttonReplace = null;
+let $buttonLibrary = null;
 
 function doSomething() {
   console.log('hallo index');
@@ -12,17 +13,22 @@ export function destroy() {
   console.log('destroy index page');
   $buttonDetail = null;
   $buttonReplace = null;
+  $buttonLibrary = null;
 }
 
 export function init() {
   console.log('init index page');
   $buttonDetail = document.getElementById('detail-button');
   $buttonReplace = document.getElementById('replace-button');
+  $buttonLibrary = document.getElementById('library-button');
 
   $buttonDetail.onclick = () => {
     switchPage('detail', true);
   };
   $buttonReplace.onclick = () => {
+    switchPage('replace');
+  };
+  $buttonLibrary.onclick = () => {
     switchPage('replace');
   };
 
--- app/router.js
+++ app/router.js
@@ -1,9 +1,11 @@
 import * as index from './pages/index';
 import * as detail from './pages/detail';
 import * as replace from './pages/replace';
+import * as library from './pages/library';
 
 export default {
   index,
   detail,
   replace,
+  library,
 };
